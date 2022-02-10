4d65fc02651f635f4a8d660f565323cc4d89c57b
Elyes <elyess.laiche@gmail.com>
2022-02-10 23:13:05 +0100

    Correctifs et ajout de requirements.txt

--- InterfaceGraphique.py
+++ InterfaceGraphique.py
@@ -1,12 +1,10 @@
-
-#Interface graphique
+# Interface graphique
 from tkinter import ttk
-#Import pour utilisation des méthodes de Gamelogic.py
+# Import pour utilisation des méthodes de Gamelogic.py
 from GameLogic import *
 
-
 def init():
-    #declaration de la fenetre du jeu
+    # declaration de la fenetre du jeu
     fenetre = tk.Tk()
     fenetre.title("Snake")
     fenetre.geometry("700x700")
@@ -19,58 +17,73 @@ def init():
     # Centre la fenetre
     fenetre.geometry("+{}+{}".format(positionRight, positionDown))
 
-    #déclaration du plateau de jeu
+    # déclaration du plateau de jeu
     Canvas = tk.Canvas(fenetre, width=700, height=650, bg="black")
-    Canvas.pack(side="bottom",fill=tk.NONE)
-    
-    button = tk.Button(fenetre, text = 'Recommencer la partie ', command =restart_program,fg='black', bg='#ffffff', activebackground='red', padx=15)
-    button.pack(side = tk.BOTTOM)
+
+    # Déclaration du bouton recommencer la partie
+    button = tk.Button(fenetre, text='Recommencer la partie ', command=reinitialiser_jeu, fg='black',
+                       bg='#ffffff', activebackground='red', padx=15)
 
     # On crée un Canvas pour le score
     Bar = tk.Text(fenetre, width=500, height=50, bg="light blue")
+
     # On écrit le score initial dans la barre
     Bar.tag_configure("tag_name", justify='center')
-    Bar.insert("1.0","score: 0\n")
+    Bar.insert("1.0", "score: 0\n")  # Centrer le texte horizontalement
     Bar.tag_add("tag_name", "1.0", "end")
-    # On place la barre
-    Bar.pack()
+
+    # Configurer le score comme read-Only
     Bar.config(state=tk.DISABLED)
+
+    # Affichage des elements declarés sur la fenetre*
+    Canvas.pack(side="bottom", fill=tk.NONE)
+    Bar.pack()
+    button.pack()
+
+    # Affecter les events aux fleches directionnelles
     fenetre.bind("<Left>", left_key)
     fenetre.bind("<Right>", right_key)
     fenetre.bind("<Up>", up_key)
     fenetre.bind("<Down>", down_key)
+
+    # affecter une tache a effectuer sur la fenetre tout le temps
     fenetre.after(0, lambda: tache(fenetre, Canvas, Bar))
-    globals.initializeGlobalVar(Canvas)
+
     return fenetre
 
-def restart_program():
-    reinitialiser_jeu()
 
 def initConfigWindow():
+    # declaration de la fenetre de configuration du jeu
     fenetreConfig = tk.Tk()
     fenetreConfig.geometry("500x500")
-    fenetreConfig.config(bg='black')
+    fenetreConfig.config(bg='grey')
     fenetreConfig.title("Snake config")
     fenetreConfig.resizable(False, False)
-    # Gets both half the screen width/height and window width/height
+
+    # Pour obtenir les positions centrales de l'ecran
     positionRight = int(fenetreConfig.winfo_screenwidth() / 2 - 250)
     positionDown = int(fenetreConfig.winfo_screenheight() / 2 - 250)
 
-    # Positions the window in the center of the page.
+    # Centre la fenetre
     fenetreConfig.geometry("+{}+{}".format(positionRight, positionDown))
-    name = tk.Label(fenetreConfig, text="Player name", fg='white', bg='black')
+
+    # Déclaration du champ texte pour pouvoir entrer le nom du joueur
+    name = tk.Label(fenetreConfig, text="Player name", fg='Black', bg='grey')
     nameEntered = tk.Text(fenetreConfig, height=1, width=25, bg='white')
+
+    # Affichage du champ texte centré horizontalement
     name.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
     nameEntered.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
+    # Déclaration des boutons pour commencer le jeu et quitter le jeu
     buttonStart = tk.Button(fenetreConfig, text='start', fg='green', bg='darkgrey', activebackground='green',
-    activeforeground='white', command=lambda: startGame(fenetreConfig,nameEntered))
+                            activeforeground='white', command=lambda: startGame(fenetreConfig, nameEntered))
     buttonQuit = tk.Button(fenetreConfig, text='quit', fg='red', bg='darkgrey', activebackground='red',
-    activeforeground='white', command=fenetreConfig.destroy)
+                           activeforeground='white', command=fenetreConfig.destroy)
+
+    #Affichage des boutons sur la fenetre
     buttonStart.place(relx=0.5, rely=0.85, anchor=tk.CENTER)
     buttonQuit.place(relx=0.5, rely=0.95, anchor=tk.CENTER)
-
-
 
     style = ttk.Style(fenetreConfig)
     # set ttk theme to "clam" which support the fieldbackground option
@@ -115,16 +128,13 @@ def LastWindow():
     return 0
 
 
-def startGame(fenetre,Textbox):
+def startGame(fenetre, Textbox):
     global NAME
     NAME = Textbox.get("1.0", tk.END)
     fenetre.destroy()
     fenetreGame = init()
     fenetreGame.mainloop()
 
-
-
-
-#S'inspirer de test.py pour faire le serpent, comment il se deplace, comment les fruits sont générés (Elyes)
-#fenetre après defaite pour afficher le top 3 des meilleurs scores, et ton personal best (Elyes)
-#modifier l'interface graphique(Pascal), faire des skins de serpent(Pascal), nom du joueur(Elyes), pouvoir enregistrer le score a la fin(Elyes), pouvoir enregistrer la partie en cours(Elyes), mode de difficulté avec la vitesse (Pascal),
+# S'inspirer de test.py pour faire le serpent, comment il se deplace, comment les fruits sont générés (Elyes)
+# fenetre après defaite pour afficher le top 3 des meilleurs scores, et ton personal best (Elyes)
+# modifier l'interface graphique(Pascal), faire des skins de serpent(Pascal), nom du joueur(Elyes), pouvoir enregistrer le score a la fin(Elyes), pouvoir enregistrer la partie en cours(Elyes), mode de difficulté avec la vitesse (Pascal),
